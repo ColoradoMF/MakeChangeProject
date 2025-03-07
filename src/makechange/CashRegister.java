d50746bf291bb1ccb3e0d302983234085f38257c
MF <michael.francavilla@gmail.com>
2025-03-07 15:57:02 -0700

    first commit

---
+++ src/makechange/CashRegister.java
@@ -0,0 +1,17 @@
+package makechange;
+
+public class CashRegister {
+	public static void main(String[] args) {
+			Scanner keyboard = new Scanner(System.in);
+			
+			double amountDue;
+			
+			double amountTendered;
+			
+			double changeDue;
+			
+			
+			
+	}
+
+}
